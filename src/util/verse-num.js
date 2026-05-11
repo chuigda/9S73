14b71c6fdeead2dde656c6cc5fe6c6c6b457c85a
Chuigda Whitegive <chuigda@doki7.club>
2026-05-11 13:01:35 +0800

    新增 verse-num.js 文件以解析古兰经经文编号

---
+++ src/util/verse-num.js
@@ -0,0 +1,32 @@
+export const parseVerseNumber = verseNumber => {
+    const [part1, part2] = verseNumber.split(':')
+    const chapter = Number(part1)
+    if (isNaN(chapter) || chapter <= 0) {
+        return '章节号无效'
+    }
+
+    if (!part2.includes('-')) {
+        const verse = Number(part2)
+        if (isNaN(verse) || verse <= 0) {
+            return '经节号无效'
+        }
+
+        return [chapter, verse, verse + 1]
+    }
+
+    const [part21, part22] = part2.split('-')
+    const startVerse = Number(part21)
+    if (isNaN(startVerse) || startVerse <= 0) {
+        return '起始经节号无效'
+    }
+    const endVerse = Number(part22)
+    if (isNaN(endVerse) || endVerse <= 0) {
+        return '结束经节号无效'
+    }
+
+    if (endVerse < startVerse) {
+        return [chapter, endVerse, startVerse + 1]
+    } else {
+        return [chapter, startVerse, endVerse + 1]
+    }
+}
